612cdf998cab9cf3200f90e1d92299d3fc910d6b
Pali Thabo <tpali@cut.ac.za>
2020-06-26 16:17:55 +0200

    Added stylesto movies

--- src/components/Movies/Movie/index.tsx
+++ src/components/Movies/Movie/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import './styles.css'
 
-const Movie: React.FC = props => {
-    return null
+type Props = {
+    title: string
+    year: string
+    image: string
+}
+
+const Movie: React.FC<Props> = props => {
+    return <div className="movie">
+        <h2>{props.title}</h2>
+        <img src={props.image} />
+        <h3>{props.year}</h3>
+    </div>
 }
 
 export default Movie
--- src/components/Movies/index.tsx
+++ src/components/Movies/index.tsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import Movie from './Movie'
+import './styles.css'
+import { CircularProgress } from '@material-ui/core'
 
 const API_KEY = 'bf45066e'
 
@@ -7,18 +10,33 @@ const series = ['avengers', 'guardians of the galaxy', 'thor', 'pirates of the c
 const Movies: React.FC = props => {
     const [movies, setMovies] = useState([])
     useEffect(() => {
-     const promises = series.map(series => {
-            fetch(`http://www.omdbapi.com/?s=${encodeURIComponent(series)}&apikey=${API_KEY}&page=1`)
+        const promises = series.map(series => {
+            return fetch(`http://www.omdbapi.com/?s=${encodeURIComponent(series)}&apikey=${API_KEY}&page=1`)
                 .then(res => res.json())
         })
 
+        Promise.all(promises).then((movies: any) => {
+            setMovies(movies.map((movie: any) => movie.Search))
+        })
 
-    Promise.all(promises).then((movies: any) =>  {
-        setMovies(movies)
-    })
+    }, [])
 
-}, [])
-    return null
+if(movies.length === 0) {
+    return <div className="loader">
+         <CircularProgress />
+    </div>
+}
+
+    return <div className="movies">
+        {movies.flat(2).map((movie: any) => {
+            return <Movie
+                key={movie.imdbID}
+                title={movie.Title}
+                year={movie.Year}
+                image={movie.Poster}
+            />
+        })}
+    </div>
 }
 
 export default Movies
